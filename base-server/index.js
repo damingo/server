e04af0f1fb8e7c67fb976223c178ca1a6ff543b9
Daniel <daniel@ansorgs.de>
2021-07-28 18:33:31 +0200

    Enable websocket compression

--- base-server/index.js
+++ base-server/index.js
@@ -374,7 +374,30 @@ export class BaseServer {
       throw new Error('You must set authentication callback by server.auth()')
     }
     this.http = await createHttpServer(this.options)
-    this.ws = new WebSocket.Server({ server: this.http })
+    // this.ws = new WebSocket.Server({ server: this.http })
+    this.ws = new WebSocket.Server({
+      perMessageDeflate: {
+        zlibDeflateOptions: {
+          // See zlib defaults.
+          chunkSize: 1024,
+          memLevel: 7,
+          level: 3
+        },
+        zlibInflateOptions: {
+          chunkSize: 10 * 1024
+        },
+        // Other options settable:
+        clientNoContextTakeover: true, // Defaults to negotiated value.
+        serverNoContextTakeover: true, // Defaults to negotiated value.
+        serverMaxWindowBits: 10, // Defaults to negotiated value.
+        // Below options specified as default values.
+        concurrencyLimit: 10, // Limits zlib concurrency for perf.
+        threshold: 1024 // Size (in bytes) below which messages
+        // should not be compressed.
+      }
+      server: this.http 
+    })
+    
     if (!this.options.server) {
       await new Promise((resolve, reject) => {
         this.ws.on('error', reject)
